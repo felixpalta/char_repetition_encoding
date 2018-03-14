567b58e92663a4eaee5714bd9940cc92429fd5be
Felix Palta (office) <fpalta90@gmail.com>
2018-03-14 17:12:57 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+
+using StringPair = std::pair<std::string, std::string>;
+
+static const StringPair TestData[] = {
+    {"", ""},
+    {"A", "A"},
+    {"AA", "A2"},
+    {"AAA", "A3"},
+    {"ABC", "ABC"},
+    {"AAAAAAAAAAAAA", "A13"},
+    {"AABBBCDDXY", "A2B3CD2XY"},
+    {"AAAAAAAAAAAABBBCCCCCCCCCCCCCCCCCDDXY", "A12B3C17D2XY"},
+};
+
+void EncodeCharRepetitionsInPlace(std::string & str) {
+    if (str.empty())
+        return;
+
+    size_t shift = 0;
+    size_t cur_count = 1;
+    char cur_c = str[0];
+
+    size_t i = 1;
+    char ith_char = str[i];
+
+    for (; i < str.size(); ++i) {
+        ith_char = str[i];
+        if (str[i] != cur_c) {
+            if (cur_count == 1) {
+                str[i - shift] = str[i];
+                cur_c = str[i];
+            } else {
+                std::string num_str = std::to_string(cur_count);
+                shift += cur_count - 1;
+                num_str.copy(&str[i - shift], num_str.size());
+                shift -= num_str.size();
+                str[i - shift] = str[i];
+                cur_c = str[i];
+                cur_count = 1;
+            }
+
+        } else {
+            ++cur_count;
+        }
+    }
+
+    if (cur_count != 1) {
+        shift += cur_count - 1;
+        std::string num_str = std::to_string(cur_count);
+
+        num_str.copy(&str[i - shift], num_str.size());
+        shift -= num_str.size();
+    }
+    str.resize(str.size() - shift);
+}
+
+int main()
+{
+    for (auto sp : TestData) {
+        std::string orig = sp.first;
+        std::string expected = sp.second;
+        auto orig_copy(orig);
+
+        EncodeCharRepetitionsInPlace(orig);
+        if (orig != expected) {
+            std::cerr << "EncodeCharRepetitionTest failed: string '" << orig_copy << "' was encoded to '" << orig << "', but expected '" << expected << "'" << std::endl;
+            return 1;
+        }
+    }
+    std::cout << "EncodeCharRepetitionTest successful!" << std::endl;
+    return 0;
+}
